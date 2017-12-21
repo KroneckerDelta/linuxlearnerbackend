17d53af104b5a03c8e6681ba5ba22e96ae523828
Thomas Michael <thomas.michael@god.de>
2017-12-21 08:48:07 +0100

    init

---
+++ src/main/java/de/mic/linuxlearner/LinuxLearnerApp.java
@@ -0,0 +1,21 @@
+package de.mic.linuxlearner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan
+@Configuration
+@EnableAutoConfiguration
+public class LinuxLearnerApp {
+
+	private static final Logger log = LoggerFactory.getLogger(LinuxLearnerApp.class);
+
+	public static void main(String args[]) {
+		SpringApplication.run(LinuxLearnerApp.class);
+	}
+
+}
