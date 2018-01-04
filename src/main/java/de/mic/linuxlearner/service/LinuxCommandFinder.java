110789bb8a266622d7c4b9e1b5d339b5d2c31980
Thomas Michael <thomas.michael@god.de>
2018-01-04 19:43:07 +0100

    binding sort and sed

--- src/main/java/de/mic/linuxlearner/service/LinuxCommandFinder.java
+++ src/main/java/de/mic/linuxlearner/service/LinuxCommandFinder.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Service;
 
 import de.mic.linuxcommand.Grep;
 import de.mic.linuxcommand.LinuxCommand;
+import de.mic.linuxcommand.Sed;
+import de.mic.linuxcommand.Sort;
 import de.mic.linuxcommand.WC;
 
 @Service
@@ -19,7 +21,10 @@ public class LinuxCommandFinder
                 return new Grep(linuxCommand);
             case "wc":
                 return new WC(linuxCommand);
-
+            case "sed":
+                return new Sed(linuxCommand);
+            case "sort":
+                return new Sort(linuxCommand);
             default:
                 return new WC(linuxCommand);
         }
